d4ca9302ecdce8ad31cc3d867289b572ae74d351
yapcheahshen <yapcheahshen@gmail.com>
2017-05-13 17:13:28 +0800

    small fixes

---
+++ buildscript/ksana-bundle.cmd
@@ -0,0 +1,2 @@
+browserify -g [envify --NODE_ENV 'production'] -r ksana-corpus-rom -r ksana-corpus -r ksana-corpus-search -x react-native | uglifyjs --screw-ie8 -c=dead_code,evaluate,loops,unused -m > ../static/ksana-bundle.min.js
+rem browserify -g [envify --NODE_ENV 'production'] -r ksana-corpus-rom -r ksana-corpus -r ksana-corpus-search -x ksana-corpus-lib -x react-native  > ../static/ksana-bundle.js
